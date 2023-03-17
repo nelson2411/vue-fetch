92cb0be006df9ae9dd02f6c1e78bb4bfc3282a55
Nelson Rosales <nelson.rosales83@gmail.com>
2023-03-16 22:29:27 -0600

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <GamesList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GamesList from "./components/GamesList.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    GamesList,
+  },
 }
 </script>
 
---
+++ src/components/GamesList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3>Games List</h3>
+    <div v-for="game in games" :key="game.id">
+      <p>{{ game.name }}</p>
+      <p>{{ game.price }}</p>
+      <p>{{ game.genre }}</p>
+      <img :src="game.image" alt="game image" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: "GamesList",
+  data() {
+    return {
+      games: [],
+    }
+  },
+  created() {
+    let url =
+      "https://my-json-server.typicode.com/nelson2411/typescript-redux-wishlist-project/videogames/"
+    axios.get(url).then((response) => {
+      this.games = response.data
+    })
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
